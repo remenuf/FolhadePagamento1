58d618e42fb6b1ca220f12996292d0e4251888c3
remenuf <42063179+remenuf@users.noreply.github.com>
2019-06-05 20:04:52 -0300

    Update Main.java

--- Folha de Pagamento/src/com/company/Main.java
+++ Folha de Pagamento/src/com/company/Main.java
@@ -181,6 +181,9 @@ public class Main {
         int j;
         for (i = 0; i < 10; i++) {
             for (j = 0; j < 11; j++) {
+                if (j == 5){
+                    continue;
+                }
                 copy[i][j] = funcionario[i][j];
             }
         }
